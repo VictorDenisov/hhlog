327c080455208f432aee611887f476a6d8028ab4
Victor Denisov <denisovenator@gmail.com>
2021-10-25 21:09:13 -0700

    Rename adif FieldPrinter

--- adif.go
+++ adif.go
@@ -7,7 +7,7 @@ import (
 func renderAdif(getters []FieldGetter, contacts []Contact) {
 	for _, c := range contacts {
 		for i, g := range getters {
-			fp := &FieldPrinter{}
+			fp := &AdifFieldPrinter{}
 			g.get(&c)
 			g.accept(fp)
 			if i > 0 {
@@ -19,41 +19,41 @@ func renderAdif(getters []FieldGetter, contacts []Contact) {
 	}
 }
 
-type FieldPrinter struct {
+type AdifFieldPrinter struct {
 	field string
 	val   string
 }
 
-func (v *FieldPrinter) printField() {
+func (v *AdifFieldPrinter) printField() {
 	fmt.Printf("<%v:%v>%v\n", v.field, len(v.val), v.val)
 }
 
-func (v *FieldPrinter) visitFrequency(g *FrequencyGetter) {
+func (v *AdifFieldPrinter) visitFrequency(g *FrequencyGetter) {
 	v.field = "FREQ"
 	v.val = g.val
 }
 
-func (v *FieldPrinter) visitCall(g *CallGetter) {
+func (v *AdifFieldPrinter) visitCall(g *CallGetter) {
 	v.field = "CALL"
 	v.val = g.val
 }
 
-func (v *FieldPrinter) visitDate(g *DateGetter) {
+func (v *AdifFieldPrinter) visitDate(g *DateGetter) {
 	v.field = "QSO_DATE"
 	v.val = g.val
 }
 
-func (v *FieldPrinter) visitTime(g *TimeGetter) {
+func (v *AdifFieldPrinter) visitTime(g *TimeGetter) {
 	v.field = "TIME_ON"
 	v.val = g.val
 }
 
-func (v *FieldPrinter) visitMode(g *ModeGetter) {
+func (v *AdifFieldPrinter) visitMode(g *ModeGetter) {
 	v.field = "MODE"
 	v.val = g.val
 }
 
-func (v *FieldPrinter) visitBand(g *BandGetter) {
+func (v *AdifFieldPrinter) visitBand(g *BandGetter) {
 	v.field = "BAND"
 	v.val = g.val
 }
