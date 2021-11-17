bad5ca52beac142c7b093b794454b53df67e9f20
Victor Denisov <denisovenator@gmail.com>
2021-11-16 18:08:03 -0800

    Use templateHeaders array

--- template.go
+++ template.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"strings"
 )
@@ -99,82 +98,25 @@ func parseReadingTemplate(line string) ([]FieldSetter, error) {
 	verbs := strings.Split(line, "\t")
 	setters := make([]FieldSetter, len(verbs))
 	for i, v := range verbs {
-		switch v {
-		case FREQUENCY:
-			setters[i] = FrequencySetter
-		case CALL:
-			setters[i] = CallSetter
-		case DATE:
-			setters[i] = DateSetter
-		case TIME:
-			setters[i] = TimeSetter
-		case MODE:
-			setters[i] = ModeSetter
-		case SRX:
-			setters[i] = SrxSetter
-		case STX:
-			setters[i] = StxSetter
-		case PREC:
-			setters[i] = PrecSetter
-		case CK:
-			setters[i] = CkSetter
-		case SECT:
-			setters[i] = SectSetter
-		default:
+		templateHanlder, ok := templateHandlers[v]
+		if !ok {
 			return nil, fmt.Errorf("Unknown verb: %v", v)
 		}
+		setters[i] = templateHanlder.setter()
 	}
 	return setters, nil
 }
 
 func parseWritingTemplate(line string) ([]FieldGetter, error) {
 
-	var skccDb *SkccDB = nil
-
 	verbs := strings.Split(line, " ")
 	getters := make([]FieldGetter, len(verbs))
 	for i, v := range verbs {
-		switch v {
-		case FREQUENCY:
-			getters[i] = &FrequencyGetter{}
-		case CALL:
-			getters[i] = &CallGetter{}
-		case DATE:
-			getters[i] = &DateGetter{}
-		case TIME:
-			getters[i] = &TimeGetter{}
-		case MODE:
-			getters[i] = &ModeGetter{}
-		case BAND:
-			getters[i] = &BandGetter{}
-		case SKCC:
-			if skccDb == nil {
-				skccDb = DownloadSkccRoster()
-			}
-			getters[i] = &SkccGetter{skccDb, ""}
-		case NAME:
-			if skccDb == nil {
-				skccDb = DownloadSkccRoster()
-			}
-			getters[i] = &NameGetter{skccDb, ""}
-		case SPC:
-			if skccDb == nil {
-				skccDb = DownloadSkccRoster()
-			}
-			getters[i] = &SpcGetter{skccDb, ""}
-		case SRX:
-			getters[i] = &SrxGetter{}
-		case STX:
-			getters[i] = &StxGetter{}
-		case PREC:
-			getters[i] = &PrecGetter{}
-		case CK:
-			getters[i] = &CkGetter{}
-		case SECT:
-			getters[i] = &SectGetter{}
-		default:
-			return nil, errors.New("Unknown verb: " + v)
+		templateHanlder, ok := templateHandlers[v]
+		if !ok {
+			return nil, fmt.Errorf("Unknown verb: %v", v)
 		}
+		getters[i] = templateHanlder.getter()
 	}
 	return getters, nil
 }
