cea356140d5bde96499d8f779661957a2f76c153
Victor Denisov <denisovenator@gmail.com>
2022-01-01 22:21:07 -0800

    Add HHLOG to allowed output formats

--- main.go
+++ main.go
@@ -96,7 +96,7 @@ The name of the input file should have the following structure: <CALLSIGN>@<PARK
 		renderHhlog(os.Stdout, getters, contacts)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown output format: %v\n", outFormat)
-		fmt.Fprintf(os.Stderr, "Allowed formats are: %v, %v\n", ADIF, CABRILLO)
+		fmt.Fprintf(os.Stderr, "Allowed formats are: %v, %v\n", ADIF, CABRILLO, HHLOG)
 		os.Exit(1)
 	}
 }
