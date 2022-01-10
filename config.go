116e43f4adc970781abd8284bc0f53274fe144a1
Victor Denisov <denisovenator@gmail.com>
2022-01-09 21:26:32 -0800

    Read config first from config dir in home dir

--- config.go
+++ config.go
@@ -8,6 +8,8 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+const configDir = ".hhlog"
+
 type Config struct {
 	Station Station `yaml:"station"`
 	Pota    Pota    `yaml:"pota"`
@@ -46,19 +48,36 @@ func parseConfig(data []byte) (*Config, error) {
 }
 
 func readConfig() (config *Config) {
-	data, err := ioutil.ReadFile(".hhlog.conf")
+	userHomeDir, err := os.UserHomeDir()
+	var data []byte
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to find user home directory: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Trying config file in the working directory\n")
+		goto workingDir
+	}
+	data, err = ioutil.ReadFile(userHomeDir + "/" + configDir + "/hhlog.conf")
+	if err == nil {
+		goto parse
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read config from home dir: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Trying config file in the working directory\n")
+	}
+workingDir:
+	data, err = ioutil.ReadFile(".hhlog.conf")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read config file:\n")
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(os.Stderr, "Proceeding without config file.\n")
+		return nil
+	}
+parse:
+	config, err = parseConfig(data)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to parse config file:\n")
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		fmt.Fprintf(os.Stderr, "Proceeding without config file.\n")
-	} else {
-		config, err = parseConfig(data)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to parse config file:\n")
-			fmt.Fprintf(os.Stderr, "%v\n", err)
-			fmt.Fprintf(os.Stderr, "Proceeding without config file.\n")
-			config = nil
-		}
+		config = nil
 	}
 	return
 }
